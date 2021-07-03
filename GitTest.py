813e9e059c6a0cb0ba439d3794f05c723946a3b4
Sivakumar-Thangapazham <63736303+Sivakumar-Thangapazham@users.noreply.github.com>
2021-07-03 23:16:09 +0100

    Add files via upload

---
+++ GitTest.py
@@ -0,0 +1,8 @@
+#print('This is a Git Test python program')
+
+ins = input('is this your first git activity? ')
+if ins == 'yes':
+    print('Good')
+    print('wow.....')
+else:
+    print('Try again')
